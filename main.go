dedde4f9dc531d28f1458c22bef12d3256d2513b
Nick Kubala <nkubala@google.com>
2019-02-15 15:12:32 -0800

    display new port forward info, and refactor a bit

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
 	"github.com/GoogleContainerTools/skaffold/pkg/skaffold/event/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 
 	"google.golang.org/grpc"
 )
@@ -17,24 +19,27 @@ var (
 	MAX_RETRIES = 10
 )
 
-func main() {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
-	if err != nil {
-		fmt.Printf("error opening connection: %s\n", err.Error())
-		os.Exit(1)
+func GetState(ctx context.Context, client proto.SkaffoldServiceClient) {
+	for {
+		r, err := client.GetState(ctx, &empty.Empty{})
+		if err != nil {
+			fmt.Printf("error retrieving state: %v\n", err)
+		}
+		fmt.Printf("Build state: %+v\n", r.BuildState)
+		fmt.Printf("Deploy state: %+v\n", r.DeployState)
+		for containerName, entry := range r.ForwardedPorts {
+			fmt.Printf("Port Forward entry for container %s: %+v\n", containerName, entry)
+		}
+		// for _, p := range r.ForwardedPorts {
+		// 	fmt.Printf("Forwarded port: %+v\n", p)
+		// }
+		time.Sleep(1 * time.Second)
 	}
-	defer conn.Close()
-	client := proto.NewSkaffoldServiceClient(conn)
+}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// r, err := client.GetState(ctx, &empty.Empty{})
-	// if err != nil {
-	// 	fmt.Printf("error retrieving state: %v\n", err)
-	// }
-	// fmt.Printf("response from server: %+v\n", r)
+func EventLog(ctx context.Context, client proto.SkaffoldServiceClient) {
 	retries := 0
+	var err error
 	var stream proto.SkaffoldService_EventLogClient
 	for {
 		stream, err = client.EventLog(ctx)
@@ -65,22 +70,34 @@ func main() {
 			fmt.Printf("%+v\n", entry)
 		}
 	}
-	// waitc := make(chan struct{})
-	// go func() {
-	// 	for {
-	// 		entry, err := stream.Recv()
-	// 		if err == io.EOF {
-	// 			close(waitc)
-	// 			return
-	// 			// break
-	// 		}
-	// 		if err != nil {
-	// 			fmt.Printf("error receiving message from stream: %v\n", err)
-	// 		}
-	// 		fmt.Printf("%+v\n", entry)
-	// 	}
-	// }()
-	// // stream.CloseSend()
-	// <-waitc
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			entry, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				fmt.Printf("error receiving message from stream: %v\n", err)
+			}
+			fmt.Printf("%+v\n", entry)
+		}
+	}()
+	<-waitc
+}
 
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("error opening connection: %s\n", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+	client := proto.NewSkaffoldServiceClient(conn)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	GetState(ctx, client)
+	// EventLog(ctx, client)
 }
